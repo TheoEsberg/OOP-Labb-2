12d1f275dec9a5ff2c67e0ebb5f57b1b5c47c1a9
Theo Esberg <esbergtheo@gmail.com>
2022-11-03 15:00:02 +0100

    Added the Animal class

---
+++ OOP-Labb-2/Animal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Labb_2
+{
+    internal class Animal
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Sound { get; set; }  
+        public string Color { get; set; }
+
+        public Animal(int age, string name, string description, string sound, string color)
+        {
+            this.Age = age;
+            this.Name = name;
+            this.Description = description;
+            this.Sound = sound;
+            this.Color = color;
+        }
+
+        public void Eat() { Console.WriteLine("{0} åt lite mat.", this.Name); }
+        public void MakeSound() { Console.WriteLine("{0}!", this.Sound); }
+        public void GetAge() { Console.WriteLine("{0} är {1} år gammal.", this.Name, this.Age); }
+        
+    }
+}
