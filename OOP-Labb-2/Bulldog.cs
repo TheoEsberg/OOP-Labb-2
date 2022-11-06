f95e9c837e041bddc65b37a1a4956db7f78388e8
Theo Esberg <esbergtheo@gmail.com>
2022-11-06 21:10:22 +0100

    Added two new dogs that inherit from the Dog class.

---
+++ OOP-Labb-2/Bulldog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Labb_2
+{
+    internal class Bulldog : Dog
+    {
+
+        protected bool IsEasyToTrain = false;
+        
+        public Bulldog(int age, string name, string description, string sound, string color, bool isFriendly, bool isEasyToTrain) 
+            : base(age, name, description, sound, color, isFriendly)
+        {
+            this.IsEasyToTrain = isEasyToTrain;
+        }
+
+        public void Train() {
+            if (!this.IsEasyToTrain) {
+                Console.WriteLine("{0} the bulldog did not want to train.", this.Name);
+            } else {
+                Console.WriteLine("{0} would gladly train.", this.Name);
+            }
+        }
+    }
+}
---
+++ OOP-Labb-2/Chihuahua.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Labb_2
+{
+    internal class Chihuahua : Dog
+    {
+
+        protected int ProbabilityToKillYou = 100;
+
+        public Chihuahua(int age, string name, string description, string sound, string color, bool isFriendly, int probabilityToKillYou)
+            : base(age, name, description, sound, color, isFriendly)
+        {
+            this.ProbabilityToKillYou = probabilityToKillYou;
+        }
+
+        public void WillItKillMe() {
+            if (this.ProbabilityToKillYou < 50) {
+                Console.WriteLine("Well yeah. {0} the bastard will probably kill you!", this.Name);
+            } else if (this.ProbabilityToKillYou >= 75)
+            {
+                Console.WriteLine("Yes! You are fucking dead, {0} will kill you!", this.Name);
+            } else {
+                Console.WriteLine("No you are okay, {0} is a friendly thing!", this.Name);
+            }
+        }
+    }
+}
