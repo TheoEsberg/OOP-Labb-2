d6590966e935d35fbce59a57675fbf77bf05bbab
Theo Esberg <esbergtheo@gmail.com>
2022-11-06 21:28:30 +0100

    Updated the Program class so that the program actually do something.

--- OOP-Labb-2/Program.cs
+++ OOP-Labb-2/Program.cs
@@ -6,7 +6,27 @@ namespace OOP_Labb_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            App();
+        }
+
+        private static void App()
+        {
+            // Instantiate three animal classes that inherit properties from the class Animal
+            Dog dog = new Dog(2, "Doggo", "This is a dog called doggo, he is a nice dog.", "Woof", "Brown", true);
+            Cat cat = new Cat(12, "Ariana", "This cat is named after ariana grandes roll 'cat' in a series called 'sam and cat'.", "Meow", "White", true);
+            Monkey monkey = new Monkey(8, "Donkey Kong", "The monkey Donkey Kong is a strong and stupid monkey.", "Ooooh Ooooh Aaaah Aaaah", "Black", 4);
+
+            // Intantiate two different dogs that inherit from the Dog class
+            Bulldog bulldog = new Bulldog(4, "Ford", "This is a Bulldog called Ford, he is named after the contry artist Colt Ford.",
+                "*woof* Contry as Truck *woof*", "Orange", true, true);
+            Chihuahua chihuahua = new Chihuahua(7, "Karen", "This dog is the definition of hell",
+                "Wof wof wooof wof wof wooof", "Gray", true, 100);
+
+            dog.MakeSound();
+            cat.MakeSound();
+            monkey.MakeSound();
+            bulldog.MakeSound();
+            chihuahua.MakeSound();
         }
     }
 }
